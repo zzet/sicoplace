0f4208bc08d9ec5cfc33693efb59909aa118f615
Andrey Kumanyaev <me@zzet.org>
2014-01-10 20:43:36 +0400

    Add clean rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sicoplace::Application.config.secret_key_base = '013b2a53b25b37d1fe3c58f0e20e4655896937c956fa3f7faf4c6f4f6f19e036a9d928e9566bc1ae840ef9db91f33fce56f69ade3b4f413d21c94667cce05dfe'
